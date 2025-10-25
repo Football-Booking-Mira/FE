39d159a79a67890ac40d88a8612630dfcb5f50b6
Phạm Viêt Anh <vietanhpham9211@gmail.com>
2025-10-26 06:27:04 +0700

    đổ dữ liệu trang chủ

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,22 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
+import { fileURLToPath } from "url";
+
+// Cần dòng này để tạo __dirname trong ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@routes": path.resolve(__dirname, "./src/routes"),
+      "@common": path.resolve(__dirname, "./src/common"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
     },
   },
 });
