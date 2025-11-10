bf88739f9a67eef70fb9914d38fa8d246af1a34e
Trinh Quoc Hung <trinhquochungwork@gmail.com>
2025-11-10 19:21:52 +0700

    Đọc user từ localStorage (token nằm trong user) và dồng bộ user,Gộp token vào object user để tiện sử dụng

--- src/common/hooks/useLogin.ts
+++ src/common/hooks/useLogin.ts
@@ -1,59 +1,84 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import api from '@/common/utils/api';
-import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { message } from 'antd';
 import { useAuth } from '@/common/contexts';
 import type { ApiError } from '@/common/utils/formApiErr';
 
 export type ILoginPayload = {
-    email: string, password: string
-}
+    email: string;
+    password: string;
+};
 
 export type ILoginResponse = {
-    message: string
+    message: string;
     data: {
         user: {
-            _id: string
-            name: string
-            phone: string
-            email: string
-            password: string
-            role: string
-            status: string
-            avatar: string
-        }
-        accessToken: string
-    }
-}
+            _id: string;
+            name: string;
+            phone: string;
+            email: string;
+            password: string;
+            role: string;
+            status: string;
+            avatar: string;
+        };
+        accessToken: string;
+    };
+};
 
-export const useLogin = (setStateOnSuccess: ()=>void, handleErrMessage: (errors: ApiError[]) => string) => {
-    const queryClient =  useQueryClient()
+export const useLogin = (
+    setStateOnSuccess: () => void,
+    handleErrMessage: (errors: ApiError[]) => string
+) => {
+    const queryClient = useQueryClient();
     const { setIsAuthenticated, setUserName, setUserRole } = useAuth();
 
     return useMutation({
-    mutationFn: async (values: ILoginPayload) => {
-            const response = await api.post<ILoginPayload, {data: ILoginResponse}>('/auth/login', values);
+        mutationFn: async (values: ILoginPayload) => {
+            const response = await api.post<ILoginPayload, { data: ILoginResponse }>(
+                '/auth/login',
+                values
+            );
             return response.data;
         },
-    onSuccess(data) {
-        localStorage.setItem("token", data.data.accessToken)
-        localStorage.setItem("user", JSON.stringify(data.data.user));
+        // onSuccess(data) {
+        //     const userWithToken = {
+        //         ...data.data.user,
+        //         token: data.data.accessToken,
+        //     };
+        //     setIsAuthenticated(true);
+        //     setUserName(data.data.user.name);
+        //     setUserRole(data.data.user.role);
 
-        setIsAuthenticated(true);
-        setUserName(data.data.user.name);
-        setUserRole(data.data.user.role);
+        //     message.success(data.message);
+        //     queryClient.resetQueries();
+        //     setStateOnSuccess();
+        // },
+        onSuccess(data) {
+            // Gộp token vào object user để tiện sử dụng
+            const userWithToken = {
+                ...data.data.user,
+                token: data.data.accessToken,
+            };
 
-        message.success(data.message);
-        queryClient.resetQueries()
-        setStateOnSuccess();
-    },
-    onError(res) {
-      if ((res as any).errors) {
-        const errMsg = handleErrMessage((res as any).errors as ApiError[]);
-        message.error(errMsg);
-        return;
-      }
-        message.error(res.message || "Đăng nhập không thành công!");
-        }
-    })
-}
+            localStorage.setItem('user', JSON.stringify(userWithToken));
+
+            setIsAuthenticated(true);
+            setUserName(data.data.user.name);
+            setUserRole(data.data.user.role);
+
+            message.success(data.message);
+            queryClient.resetQueries();
+            setStateOnSuccess();
+        },
+
+        onError(res) {
+            if ((res as any).errors) {
+                const errMsg = handleErrMessage((res as any).errors as ApiError[]);
+                message.error(errMsg);
+                return;
+            }
+            message.error(res.message || 'Đăng nhập không thành công!');
+        },
+    });
+};
