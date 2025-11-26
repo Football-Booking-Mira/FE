360670a85ef07d40d69739248efb9d912b600b09
Manh nt <nguyentienmanh030@gmail.com>
2025-11-26 15:28:20 +0700

    create: profile user client

--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -8,6 +8,7 @@ import Checkout from '@/pages/client/home/Checkout';
 import PitchDetail from '@/pages/client/home/PitchDetail';
 import MyBookings from '@/pages/client/home/MyBookings';
 import PaymentSuccessPage from '@/pages/client/home/PaymentSuccessPage';
+import Profile from '@/pages/client/home/Profile';
 
 export const PublicRoutes: RouteObject[] = [
     {
@@ -22,6 +23,7 @@ export const PublicRoutes: RouteObject[] = [
             { path: 'verify-email', element: <VerifyEmailPage /> },
             { path: 'reset-password', element: <ResetPasswordPage /> },
             { path: 'payment-return', element: <PaymentSuccessPage /> },
+            { path: 'profile', element: <Profile /> },
         ],
     },
 ];
